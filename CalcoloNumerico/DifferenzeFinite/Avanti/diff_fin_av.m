7534ea51991010fbd3e4a87f6e4e1f7354a76b73
Paolo Vagnini <paolondon@gmail.com>
2020-05-17 21:16:03 +0200

    docs contatti e link al canale

--- CalcoloNumerico/DifferenzeFinite/Avanti/diff_fin_av.m
+++ CalcoloNumerico/DifferenzeFinite/Avanti/diff_fin_av.m
@@ -1,3 +1,8 @@
+% * Sviluppato da Tutor Island *
+% --> Prenota le tue lezioni private per materie scientifiche: contact.tutorisland@gmail.com
+% URL del video con la spiegazione riga per riga: https://www.youtube.com/watch?v=jYgrhPudsyw
+% Iscritivi al Canale YouTube: https://www.youtube.com/channel/UCKkzN06obaHk8mt3iBTp6qw
+
 function [num_dy] = diff_fin_av(x, h)
     % creare il vettore di uscita (giuste dimensioni)
     num_dy = zeros(1, length(x)-1);
