410737d05ddf617ce8422a23dfc2a80fe5c47a00
Paolo Vagnini <paolondon@gmail.com>
2020-06-23 13:38:53 +0200

    feat TI_023_N if_else

---
+++ MATLAB_per_INGEGNERI/04_if_else/driver.m
@@ -0,0 +1,4 @@
+% questo programma calcola il massimo tra 2 numeri passati come argomento
+n1 = 0;
+n2 = 10;
+n = massimo(n1, n2)
---
+++ MATLAB_per_INGEGNERI/04_if_else/massimo.m
@@ -0,0 +1,8 @@
+% calcola il massimo tra 2 numeri passati come argomento
+function [n] = massimo(a, b)
+    if (a >= b)
+        n = a;
+    else
+        n = b;
+    end
+end
